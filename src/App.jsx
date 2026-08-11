53ef04e1b83507ef94e12c63916870518e816d0f
Brenda Jacques <brendajacques.design@gmail.com>
2026-08-10 21:32:43 -0300

    implementa componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
